e53d083346170019c13b9dae64287a3e00fa7e94
Pertempto <pertemptodisce@gmail.com>
2025-03-07 13:53:46 -0500

    feat: update app ID

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.karriba.karriba"
+    namespace = "com.karriba.mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.karriba.karriba"
+        applicationId = "com.karriba.mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- app/android/app/src/main/kotlin/com/karriba/karriba/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/karriba/karriba/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.karriba.karriba
+package com.karriba.mobile
 
 import io.flutter.embedding.android.FlutterActivity
 
