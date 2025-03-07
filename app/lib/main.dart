ee55a221a015b3730380171d4c405d4159da05b0
Pertempto <pertemptodisce@gmail.com>
2025-03-07 14:08:20 -0500

    refactor: Rename MyHomePage to KarribaHomePage and MyApp to KarribaApp

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,58 +5,32 @@ import 'applicators_page.dart';
 import 'settings_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const KarribaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class KarribaApp extends StatelessWidget {
+  const KarribaApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Karriba',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: const KarribaHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class KarribaHomePage extends StatefulWidget {
+  const KarribaHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<KarribaHomePage> createState() => _KarribaHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _KarribaHomePageState extends State<KarribaHomePage> {
   int _selectedIndex = 0;
 
   static final List<Widget> _widgetOptions = <Widget>[
