d8a516ad96902cdb8380368867a332082e3a0925
Pertempto <pertemptodisce@gmail.com>
2025-03-08 12:42:17 -0500

    chore: clean up

--- app/lib/new_applicator_page.dart
+++ app/lib/new_applicator_page.dart
@@ -5,45 +5,41 @@ class NewApplicatorPage extends StatelessWidget {
   const NewApplicatorPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        return await showUnsavedChangesDialog(context);
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('New Applicator'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: () {
-                // TODO: actually save the form input
-                Navigator.pop(context);
-              },
+  Widget build(BuildContext context) => WillPopScope(
+    onWillPop: () async => await showUnsavedChangesDialog(context),
+    child: Scaffold(
+      appBar: AppBar(
+        title: const Text('New Applicator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: () {
+              // TODO: actually save the form input
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 16,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'License Number',
+                border: OutlineInputBorder(),
+              ),
             ),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            spacing: 16,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Name',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'License Number',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
-    );
-  }
+    ),
+  );
 }
