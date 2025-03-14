9f7d7e115d4ba4966f9b2defb36687e5585ada86
Pertempto <pertemptodisce@gmail.com>
2025-03-14 09:48:04 -0400

    chore: clean up

--- app/lib/edit_applicator_page.dart
+++ app/lib/edit_applicator_page.dart
@@ -6,7 +6,7 @@ import 'package:karriba/applicator_dao.dart';
 import 'unsaved_changes_dialog.dart';
 
 class EditApplicatorPage extends StatefulWidget {
-  const EditApplicatorPage({super.key, required this.applicator});
+  const EditApplicatorPage({super.key, this.applicator});
   final Applicator? applicator;
 
   @override
@@ -28,60 +28,66 @@ class _EditApplicatorPageState extends State<EditApplicatorPage> {
 
   @override
   Widget build(BuildContext context) => WillPopScope(
-        onWillPop: () async => await showUnsavedChangesDialog(context),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(_title),
-            actions: [
-              IconButton(
-                icon: Iconify(Mdi.content_save),
-                onPressed: () => _saveApplicator(context),
+    onWillPop: () async => await showUnsavedChangesDialog(context),
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        actions: [
+          IconButton(
+            icon: Iconify(Mdi.content_save),
+            onPressed: () => _saveApplicator(context),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            spacing: 16,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+                initialValue: _draftApplicator.name,
+                onSaved:
+                    (value) =>
+                        _draftApplicator = _draftApplicator.copyWith(
+                          name: value,
+                        ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'License Number',
+                  border: OutlineInputBorder(),
+                ),
+                initialValue: _draftApplicator.licenseNumber,
+                onSaved:
+                    (value) =>
+                        _draftApplicator = _draftApplicator.copyWith(
+                          licenseNumber: value,
+                        ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a license number';
+                  }
+                  return null;
+                },
               ),
             ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                spacing: 16,
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Name',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: _draftApplicator.name,
-                    onSaved: (value) => _draftApplicator =
-                        _draftApplicator.copyWith(name: value),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a name';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'License Number',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: _draftApplicator.licenseNumber,
-                    onSaved: (value) => _draftApplicator =
-                        _draftApplicator.copyWith(licenseNumber: value),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a license number';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
         ),
-      );
+      ),
+    ),
+  );
 
   Future<void> _saveApplicator(BuildContext context) async {
     if (!_formKey.currentState!.validate()) {
