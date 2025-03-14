b4e974e907ab2c11523b1adbeeb2a033112f103f
Pertempto <pertemptodisce@gmail.com>
2025-03-14 10:47:48 -0400

    feat: Implement edit customer page with form validation and data saving

--- app/lib/edit_customer_page.dart
+++ app/lib/edit_customer_page.dart
@@ -21,120 +21,124 @@ class _EditCustomerPageState extends State<EditCustomerPage> {
 
   @override
   void initState() {
-    _draftCustomer = widget.customer ??
-        Customer(
-          name: '',
-          streetAddress: '',
-          city: '',
-          state: '',
-          zipCode: '',
-        );
+    _draftCustomer =
+        widget.customer ??
+        Customer(name: '', streetAddress: '', city: '', state: '', zipCode: '');
     _title = widget.customer == null ? 'New Customer' : 'Edit Customer';
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) => WillPopScope(
-        onWillPop: () async => await showUnsavedChangesDialog(context),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(_title),
-            actions: [
-              IconButton(
-                icon: Iconify(Mdi.content_save),
-                onPressed: () => _saveCustomer(context),
+    onWillPop: () async => await showUnsavedChangesDialog(context),
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        actions: [
+          IconButton(
+            icon: Iconify(Mdi.content_save),
+            onPressed: () => _saveCustomer(context),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            spacing: 16,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+                initialValue: _draftCustomer.name,
+                onSaved:
+                    (value) =>
+                        _draftCustomer = _draftCustomer.copyWith(name: value),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Street Address',
+                  border: OutlineInputBorder(),
+                ),
+                initialValue: _draftCustomer.streetAddress,
+                onSaved:
+                    (value) =>
+                        _draftCustomer = _draftCustomer.copyWith(
+                          streetAddress: value,
+                        ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a street address';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'City',
+                  border: OutlineInputBorder(),
+                ),
+                initialValue: _draftCustomer.city,
+                onSaved:
+                    (value) =>
+                        _draftCustomer = _draftCustomer.copyWith(city: value),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a city';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'State',
+                  border: OutlineInputBorder(),
+                ),
+                initialValue: _draftCustomer.state,
+                onSaved:
+                    (value) =>
+                        _draftCustomer = _draftCustomer.copyWith(state: value),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a state';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Zip Code',
+                  border: OutlineInputBorder(),
+                ),
+                initialValue: _draftCustomer.zipCode,
+                keyboardType: TextInputType.number,
+                onSaved:
+                    (value) =>
+                        _draftCustomer = _draftCustomer.copyWith(
+                          zipCode: value,
+                        ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a zip code';
+                  }
+                  return null;
+                },
               ),
             ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                spacing: 16,
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Name',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: _draftCustomer.name,
-                    onSaved: (value) =>
-                        _draftCustomer = _draftCustomer.copyWith(name: value),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a name';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Street Address',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: _draftCustomer.streetAddress,
-                    onSaved: (value) => _draftCustomer =
-                        _draftCustomer.copyWith(streetAddress: value),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a street address';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'City',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: _draftCustomer.city,
-                    onSaved: (value) =>
-                        _draftCustomer = _draftCustomer.copyWith(city: value),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a city';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'State',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: _draftCustomer.state,
-                    onSaved: (value) =>
-                        _draftCustomer = _draftCustomer.copyWith(state: value),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a state';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Zip Code',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: _draftCustomer.zipCode,
-                    keyboardType: TextInputType.number,
-                    onSaved: (value) => _draftCustomer =
-                        _draftCustomer.copyWith(zipCode: value),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a zip code';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
         ),
-      );
+      ),
+    ),
+  );
 
   Future<void> _saveCustomer(BuildContext context) async {
     if (!_formKey.currentState!.validate()) {
@@ -144,6 +148,7 @@ class _EditCustomerPageState extends State<EditCustomerPage> {
     _formKey.currentState!.save();
 
     final customerDao = CustomerDao();
+    // TODO: use CustomerDao.save
     await customerDao.insert(_draftCustomer);
 
     Navigator.pop(context);
