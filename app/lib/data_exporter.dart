8e29a8c122c09a90b1256d0437838a3a9b531805
Addison Emig <addison@kwila.cloud>
2025-03-15 14:57:44 -0400

    feat: data export (#47)
    * refactor: Move export logic to DataExporter class for separation
    
    * refactor: Refactor DataExporter class to a function for simplicity
    
    * Refactor: Convert DataExporter class to a bare function
    
    * feat: Export database to downloads directory without storage permission
    
    * feat: Use FlutterFileDialog for database export and add permissions
    
    * feat: data exporter
    
    * refactor: clean up
    
    * refactor: put line back
    
    * refactor: private schema version
    
    * fix: path is async
    
    * refactor: replace getter with function to reduce confusion about async

---
+++ app/lib/data_exporter.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:karriba/database_helper.dart';
+
+Future<void> exportDatabase(BuildContext context) async {
+  try {
+    Directory? downloadsDir;
+    if (Platform.isAndroid) {
+      downloadsDir = Directory("/storage/emulated/0/Download");
+    }
+    // TODO: allow the user to select the download directory
+
+    if (downloadsDir == null) {
+      throw Exception("Data export is not yet supported on this platform.");
+    }
+
+    String formattedDate = DateFormat(
+      'yyyy-MM-dd_HH-mm-ss',
+    ).format(DateTime.now());
+    String exportPath = '${downloadsDir.path}/karriba_export_$formattedDate.db';
+
+    File sourceFile = File(await DatabaseHelper.getPath());
+    await sourceFile.copy(exportPath);
+
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text('Data exported to $exportPath')));
+  } catch (e) {
+    // 6. Show error message
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text('Error exporting data: $e')));
+  }
+}
