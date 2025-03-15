84066a36be597faff513b643f095ddb28c23d2b0
Pertempto <pertemptodisce@gmail.com>
2025-03-15 12:24:03 -0400

    feat: data exporter

--- app/lib/data_exporter.dart
+++ app/lib/data_exporter.dart
@@ -1,61 +1,34 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 
 Future<void> exportDatabase(BuildContext context) async {
-  // 1. Check and request storage permission
-  var status = await Permission.storage.status;
-  if (!status.isGranted) {
-    status = await Permission.storage.request();
-    if (!status.isGranted) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Storage permission required to export data.'),
-        ),
-      );
-      return;
-    }
-  }
-
   try {
-    // 2. Get the database path
     String databasesPath = await getDatabasesPath();
     String dbPath = '$databasesPath/karriba.db';
 
-    // 3. Generate the export file path
+    Directory? downloadsDir;
+    if (Platform.isAndroid) {
+      downloadsDir = Directory("/storage/emulated/0/Download");
+    }
+    // TODO: allow the user to select the download directory
+
+    if (downloadsDir == null) {
+      throw Exception("Data export is not yet supported on this platform.");
+    }
+
     String formattedDate = DateFormat(
       'yyyy-MM-dd_HH-mm-ss',
     ).format(DateTime.now());
-    String fileName = 'karriba_export_$formattedDate.db';
+    String exportPath = '${downloadsDir.path}/karriba_export_$formattedDate.db';
 
-    final params = SaveFileDialogParams(
-      sourceFilePath: dbPath,
-      fileName: fileName,
-    );
-
-    final filePath = await FlutterFileDialog.saveFile(params: params);
-
-    if (filePath == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Export cancelled'),
-        ),
-      );
-      return;
-    }
-
-    // 4. Copy the database file
     File sourceFile = File(dbPath);
-    File destFile = File(filePath);
-    await sourceFile.copy(filePath);
+    await sourceFile.copy(exportPath);
 
-    // 5. Show success message
     ScaffoldMessenger.of(
       context,
-    ).showSnackBar(SnackBar(content: Text('Data exported to $filePath')));
+    ).showSnackBar(SnackBar(content: Text('Data exported to $exportPath')));
   } catch (e) {
     // 6. Show error message
     ScaffoldMessenger.of(
--- app/lib/settings_page.dart
+++ app/lib/settings_page.dart
@@ -5,33 +5,28 @@ import 'coming_soon_dialog.dart';
 import 'data_exporter.dart';
 
 class SettingsPage extends StatelessWidget {
-  final DataExporter dataExporter = DataExporter();
+  const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: const Iconify(Mdi.database_import),
-            title: const Text('Import Data'),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => const ComingSoonDialog(),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Iconify(Mdi.database_export),
-            title: const Text('Export Data'),
-            onTap: () => dataExporter.exportDatabase(context),
-          ),
-        ],
-      ),
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: const Iconify(Mdi.database_import),
+          title: const Text('Import Data'),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => const ComingSoonDialog(),
+            );
+          },
+        ),
+        ListTile(
+          leading: const Iconify(Mdi.database_export),
+          title: const Text('Export Data'),
+          onTap: () => exportDatabase(context),
+        ),
+      ],
     );
   }
 }
