c8e5a0c76b672ffac4b4375cb52da1e1f1ce8997
Pertempto <pertemptodisce@gmail.com>
2025-04-15 19:04:14 -0400

    feat: export to JSON file

--- app/lib/data_exporter.dart
+++ app/lib/data_exporter.dart
@@ -1,48 +1,37 @@
 import 'dart:io';
-import 'dart:js' as js;
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:karriba/database_helper.dart';
+import 'package:path/path.dart';
+import 'package:universal_html/html.dart' as html; // Import for web file saving
 
 Future<void> exportDatabase(BuildContext context) async {
   final String formattedDate = DateFormat(
     'yyyy-MM-dd_HH-mm-ss',
   ).format(DateTime.now());
+  String jsonData = await DatabaseHelper.instance.exportToJson();
+  String fileName = 'karriba_export_$formattedDate.json';
   try {
     if (kIsWeb) {
-      // Export to JSON on web
-      String jsonData = await DatabaseHelper.instance.exportToJson();
-      String fileName = 'karriba_export_$formattedDate.json';
-
-      // Create a download
-      js.context.callMethod('downloadFile', [fileName, jsonData]);
-
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('Data exported to $fileName')));
-    } else {
-      // Export to DB file on other platforms
-      Directory? downloadsDir;
-      if (Platform.isAndroid) {
-        downloadsDir = Directory("/storage/emulated/0/Download");
-      }
+      // Download the file
+      final blob = html.Blob([jsonData], 'application/json');
+      final url = html.Url.createObjectUrlFromBlob(blob);
+      html.AnchorElement(href: url)
+        ..setAttribute('download', fileName)
+        ..click();
+      html.Url.revokeObjectUrl(url);
+    } else if (Platform.isAndroid) {
       // TODO: allow the user to select the download directory
-
-      if (downloadsDir == null) {
-        throw Exception("Data export is not yet supported on this platform.");
-      }
-
-      String exportPath =
-          '${downloadsDir.path}/karriba_export_$formattedDate.db';
-
-      File sourceFile = File(await DatabaseHelper.getPath());
-      await sourceFile.copy(exportPath);
-
+      String downloadsDir = "/storage/emulated/0/Download";
+      File file = File(join(downloadsDir, fileName));
+      await file.writeAsString(jsonData);
       ScaffoldMessenger.of(
         context,
-      ).showSnackBar(SnackBar(content: Text('Data exported to $exportPath')));
+      ).showSnackBar(SnackBar(content: Text('Data exported to ${file.path}')));
+    } else {
+      throw Exception("Data export is not yet supported on this platform.");
     }
   } catch (e) {
     // Show error message
